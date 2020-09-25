5e886c063fc45ace235594927ce5aad3059ef815
Marcus Leow <marcusleow@hotmail.com>
2020-09-25 12:36:27 +0800

    added styling for login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,28 +2,49 @@ import React from "react";
 import "./Login.css";
 
 class Login extends React.Component {
+  handleSubmitevents(event) {
+    // handle submit events
+  }
+
+  handlePasswordChange(event) {
+    //handle password change events
+  }
+
   render() {
     return (
-      <div className="container-login">
+      <div>
         <div>
-          <form className="container-login">
-            <label>
-              Login :
-              <input className="loginUsername" type="text" name="username" />
-            </label>
-            <label>
-              Password :
-              <input
-                className="loginPassword"
-                type="password"
-                name="password"
-              ></input>
-            </label>
-            <label>Remember Me?</label>
-            <input type="checkbox"></input>
-
-            <button onSubmit={console.log("Submitted")}>Clear</button>
-            <button onSubmit={console.log("Submitted")}>Login</button>
+          <form className="container-form">
+            <p className="container-username">
+              <label className="container-username-label">
+                Login : {""}
+                <input type="text" name="username" />
+              </label>
+            </p>
+            <p className="container-password">
+              <label className="container-password-label">
+                Password : {""}
+                <input type="password" name="password"></input>
+              </label>
+            </p>
+            <p className="container-remember">
+              <label className="container-remember-label">Remember Me?</label>
+              <input type="checkbox"></input>
+            </p>
+            <button
+              className="clear-button"
+              style={{ float: "right" }}
+              onSubmit={() => console.log("test")}
+            >
+              Clear
+            </button>
+            <button
+              className="submit-button"
+              style={{ float: "right" }}
+              onSubmit={() => console.log("test")}
+            >
+              Login
+            </button>
           </form>
         </div>
       </div>
