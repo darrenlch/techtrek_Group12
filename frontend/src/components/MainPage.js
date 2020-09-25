7131e3c0efe017f6ef7bfe212f5b3b2cea042fc6
Jia Wei <jwlim295@gmail.com>
2020-09-25 11:35:19 +0800

    Created structure for main page layout

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import './MainPage.css';
 
 class MainPage extends React.Component{
     render(){
         return(
-            <div>
-               
+            <div className = "mainpage">
+                <h1 className = "mainpage-title">Onboarded Users</h1>
+                <button className = "btn btn-primary add-user">Add New User</button>
+                <table className = "table">
+                    <thead className = "thead">
+                        <th scope = "col">Name</th>
+                        <th scope = "col">Product Type</th>
+                        <th scope = "col">Registration Time</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                        </tr>
+                        <tr>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                        </tr>
+                        <tr>
+                            <td>Test</td>
+                            <td>Test</td>
+                            <td>Test</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
