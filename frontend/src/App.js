06d4807f26ff948d65184760443d13af12ba0738
Jia Wei <jwlim295@gmail.com>
2020-09-25 12:51:58 +0800

    Added simple routing path between different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,21 @@ import EditCustomer from  './components/EditCustomer.js';
 import AddCustomer from  './components/AddCustomer.js';
 import Navbar from './components/Navbar.js';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Login/>
-      <MainPage/>
-      <EditCustomer/>
-      <AddCustomer/>
+      <Router>
+        <Switch>
+          <Route path = '/' exact><Login/></Route>
+          <Route path = '/main'><MainPage/></Route>
+          <Route path = '/customers/edit'><EditCustomer/></Route>
+          <Route path = '/customers/add'><AddCustomer/></Route>
+        </Switch> 
+      </Router>
+     
     </div>
   );
 }
