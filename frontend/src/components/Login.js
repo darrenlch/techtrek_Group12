9f1873083e2b5792eceb721659ae8f0552571e42
Marcus Leow <marcusleow@hotmail.com>
2020-09-25 12:02:21 +0800

    Added login page sans formatting

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
+import "./Login.css";
 
-class Login extends React.Component{
-    render(){
-        return(
-            <div>
-            
-            </div>
-        )
-    }
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="container-login">
+        <div>
+          <form className="container-login">
+            <label>
+              Login :
+              <input className="loginUsername" type="text" name="username" />
+            </label>
+            <label>
+              Password :
+              <input
+                className="loginPassword"
+                type="password"
+                name="password"
+              ></input>
+            </label>
+            <label>Remember Me?</label>
+            <input type="checkbox"></input>
+
+            <button onSubmit={console.log("Submitted")}>Clear</button>
+            <button onSubmit={console.log("Submitted")}>Login</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
