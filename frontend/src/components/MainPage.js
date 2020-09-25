6bbd9333b0ca210525ea330be476994f34752d49
Jia Wei <jwlim295@gmail.com>
2020-09-25 11:55:11 +0800

    Added edit and delete buttons and restructured table for better alignment

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -7,27 +7,43 @@ class MainPage extends React.Component{
             <div className = "mainpage">
                 <h1 className = "mainpage-title">Onboarded Users</h1>
                 <button className = "btn btn-primary add-user">Add New User</button>
-                <table className = "table">
+                <table className = "table table-striped">
                     <thead className = "thead">
-                        <th scope = "col">Name</th>
-                        <th scope = "col">Product Type</th>
-                        <th scope = "col">Registration Time</th>
+                        <tr>
+                            <th scope = "col" className = "text-center">Name</th>
+                            <th scope = "col" className = "text-center">NRIC</th>
+                            <th scope = "col" className = "text-center">Registration Time</th>
+                            <th scope = "col"></th>
+                        </tr>
+                        
                     </thead>
                     <tbody>
-                        <tr>
+                        <tr className = "text-center">
                             <td>Test</td>
                             <td>Test</td>
                             <td>Test</td>
+                            <td>
+                                <button className = "btn btn-primary mr-2">Edit</button>
+                                <button className = "btn btn-danger">Delete</button>
+                            </td>
                         </tr>
-                        <tr>
+                        <tr className = "text-center">
                             <td>Test</td>
                             <td>Test</td>
                             <td>Test</td>
+                            <td>
+                                <button className = "btn btn-primary mr-2">Edit</button>
+                                <button className = "btn btn-danger">Delete</button>
+                            </td>
                         </tr>
-                        <tr>
+                        <tr className = "text-center">
                             <td>Test</td>
                             <td>Test</td>
                             <td>Test</td>
+                            <td>
+                                <button className = "btn btn-primary mr-2">Edit</button>
+                                <button className = "btn btn-danger">Delete</button>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
