3593bb4a2c2571a99be253d20f42e163d2a39b90
azmansalleh <azman_master@hotmail.com>
2020-09-25 13:52:08 +0800

    Added customer model

---
+++ frontend/src/models/Customer.js
@@ -0,0 +1,11 @@
+export class Card {
+    constructor(customerName, customerAge, serviceOfficerName, NRIC, registrationTime, branchCode, productType) {
+      this.customerName = customerName;
+      this.customerAge = customerAge;
+      this.serviceOfficerName = serviceOfficerName;
+      this.NRIC = NRIC;
+      this.registrationTime = registrationTime;
+      this.branchCode = branchCode;
+      this.productType = productType;
+    }
+}
