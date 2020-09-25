92a422fdfe8d7d605fecdccb249524c0a136201e
Jia Wei <jwlim295@gmail.com>
2020-09-25 13:39:42 +0800

    Implemented simple protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,27 @@ import MainPage from  './components/MainPage.js';
 import EditCustomer from  './components/EditCustomer.js';
 import AddCustomer from  './components/AddCustomer.js';
 import Navbar from './components/Navbar.js';
+import ProtectedRoute from './components/ProtectedRoute.js';
 import 'bootstrap/dist/css/bootstrap.css';
+import {useState} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
+
+  const [isLogged, setLogState] = useState(false);
+
   return (
     <div className="App">
       <Navbar/>
       <Router>
         <Switch>
           <Route path = '/' exact><Login/></Route>
-          <Route path = '/main'><MainPage/></Route>
+          <ProtectedRoute path = '/main' user = {isLogged} component={MainPage}></ProtectedRoute>
           <Route path = '/customers/edit'><EditCustomer/></Route>
           <Route path = '/customers/add'><AddCustomer/></Route>
         </Switch> 
       </Router>
+     
     </div>
   );
 }
