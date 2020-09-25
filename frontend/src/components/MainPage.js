fa3aa41e52a199c7d5e9262d49eb8924f65e29b3
Jia Wei <jwlim295@gmail.com>
2020-09-25 11:13:24 +0800

    Added dependencies and initial setup of components

---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+                <p>Content</p>
+            </div>
+        )
+    }
+}
+
+export default MainPage;
